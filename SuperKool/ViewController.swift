9392d8740aa9d5b4b1800ca6bf046204ee050994
Mac <mac@Macs-MBP-2.lan>
2016-02-06 23:53:51 +0300

    Added Files For Supperkool

--- SuperKool/ViewController.swift
+++ SuperKool/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var coolbut: UIButton!
+    @IBOutlet weak var coolLogo: UIImageView!
+    @IBOutlet weak var coolbg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func coolAction(sender: AnyObject) {
+        coolLogo.hidden=false
+        coolbg.hidden=false
+        coolbut.hidden=true
+    }
 
 }
 
